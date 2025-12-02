7e7c5048fc24a16a092dfd8bcfed6272eb8e49b6
triadit15 <siyabongashipalana01@gmail.com>
2025-12-02 09:59:18 +0200

    Fix merge conflict in init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -45,53 +44,3 @@ def create_app():
     app.register_blueprint(utility)
     
     return app
-=======
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_migrate import Migrate
-import os
-
-db = SQLAlchemy()
-migrate = Migrate()
-
-def create_app():
-    app = Flask(__name__)
-
-    # SECRET KEY
-    app.config["SECRET_KEY"] = "senti_super_secret_key"
-
-    # SQLITE DATABASE
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///senti.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    # INIT EXTENSIONS
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    # LOGIN MANAGER
-    login_manager = LoginManager()
-    login_manager.login_view = "main.login"
-    login_manager.init_app(app)
-
-    # IMPORT MODELS
-    from .models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
-    # BLUEPRINT REGISTRATION
-    from .routes import bp as main_bp
-    app.register_blueprint(main_bp)
-
-    from .marketplace_routes import market
-    app.register_blueprint(market)
-
-    from .utility_routes import utility
-    app.register_blueprint(utility)
-    
-
-    return app
-
->>>>>>> 7762d9128f8de91025cf4a55232aad8b63d6ce04
